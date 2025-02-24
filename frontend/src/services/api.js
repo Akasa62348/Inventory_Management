9582289e6e38bfe263cd6ada0aa33195f4ac212b
akash <akasa62348@gmail.comgit config --global user.email akasa62348@gmail.comgit config --global user.email akasa62348@gmail.com>
2025-02-24 16:58:38 +0530

    render url api

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "https://inventory-management-v976.onrender.com",
+  baseURL: import.meta.env.VITE_API_URL || "https://inventory-management-v976.onrender.com/api",
   
 });
 console.log('API Base URL:', import.meta.env.VITE_API_URL);  // Debug check
